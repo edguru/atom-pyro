43bdc65fc71c17d678e4b3832e7b8fea88fb0bbc
xChetan-xD <137705189+xChetan-xD@users.noreply.github.com>
2023-08-07 21:44:05 +0530

    Typo 2.0

--- wbb/modules/_telegraph.py
+++ wbb/modules/_telegraph.py
@@ -3,7 +3,7 @@ from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 
-soulmate = "SOULMATE"
+atom-pyro = "ATOM-PYRO"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=soulmate)
 auth_url = r["auth_url"]
