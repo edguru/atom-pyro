44da27e3eb270c2478ea36d9347ae3a05e779d87
xChetan-xD <137705189+xChetan-xD@users.noreply.github.com>
2023-08-08 19:17:49 +0530

    Update __init__.py

--- wbb/modules/sql/__init__.py
+++ wbb/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from wbb import DB_URI
+from wbb import MONGO_DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
