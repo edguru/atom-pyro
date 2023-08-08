13e975e3bb74e55304a58a2b1b878dba6956e36d
xChetan-xD <137705189+xChetan-xD@users.noreply.github.com>
2023-08-08 20:51:30 +0530

    Update __init__.py

--- wbb/modules/sql/__init__.py
+++ wbb/modules/sql/__init__.py
@@ -1,11 +1,11 @@
-from wbb import MONGO_DB_URI
+from wbb import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(MONGO_DB_URI, client_encoding="utf8")
+    engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
