146248351cd4e7b0cdd77f93e803033388ec92a5
xChetan-xD <137705189+xChetan-xD@users.noreply.github.com>
2023-08-08 09:05:25 +0530

    Update _telegraph.py

--- wbb/modules/_telegraph.py
+++ wbb/modules/_telegraph.py
@@ -3,7 +3,6 @@ from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 
-atom-pyro = "ATOM-PYRO"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=soulmate)
 auth_url = r["auth_url"]
